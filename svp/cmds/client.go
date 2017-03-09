f8eff69abc03c557ea9d45a1ec249151e35f281c
Matthew Steffen <msteffen@pachyderm.io>
2017-03-08 16:41:32 -0800

    Fix code that downloads vim-go binaries, and run it in parallel with code that downloads pachyderm repo

--- svp/cmds/client.go
+++ svp/cmds/client.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"bytes"
 	"fmt"
+	"golang.org/x/sync/errgroup"
 	"os"
 	"path"
 	"regexp"
@@ -66,66 +67,77 @@ var newClient = &cobra.Command{
 			return f.err
 		}
 
-		// Pull the pachyderm repo into the client directory
+		// Download data (pachyderm repo and vim binaries) into the new client
 		// TODO: This should be copied instead of downloaded every time. This takes
 		// several seconds to finish
+		var eg errgroup.Group
 		os.Setenv("GOPATH", clientpath)
 		os.Chdir(clientpath)
-		fmt.Println("Fetching Pachyderm repo...")
-		op := StartOp()
-		op.Run("go", "get", "github.com/pachyderm/pachyderm")
-		os.Chdir(path.Join(clientpath, "src/github.com/pachyderm/pachyderm"))
-
-		// Create a git branch matching the clientname
-		// TODO(msteffen): let the user specify the branch, in case you want to have
-		// multiple clients for the same non-master branch
-		op.Run("git", "checkout", "-b", clientname)
-		if op.LastError() != nil {
-			return fmt.Errorf("couldn't create client branch:\n%s", op.DetailedError())
-		}
-
-		// Update .git/config so that the 'origin' remote repo uses ssh instead of http
-		fmt.Println("Updating .git/config...")
-		stat, err := os.Stat("./.git/config")
-		if err != nil {
-			return fmt.Errorf("could not stat .git/config to update origin: %s", err)
-		}
-		gitconf, err := os.OpenFile("./.git/config", os.O_RDWR, 0664)
-		if err != nil {
-			return fmt.Errorf("could not open .git/config to update origin: %s", err)
-		}
-		// (we're replacing one line of config with another that's the same length)
-		out := bytes.NewBuffer(make([]byte, 0, stat.Size()))
-		scanner := bufio.NewScanner(gitconf)
-		for scanner.Scan() {
-			if scanner.Text() == "\turl = https://github.com/pachyderm/pachyderm" {
-				out.Write([]byte("\turl = git@github.com:pachyderm/pachyderm.git"))
-			} else {
-				out.Write(scanner.Bytes())
+		// Install vim-go binaries in a separate goroutine (slow)
+		eg.Go(func() error {
+			op := StartOp()
+			fmt.Println("Beginning to install vim-go binaries...")
+			op.Run("vim", "-c", "GoUpdateBinaries", "-c", "q")
+			if op.LastError() != nil {
+				return fmt.Errorf("couldn't install go binaries:\n%s", op.DetailedError())
 			}
-			out.WriteByte('\n')
-		}
-		// Replace gitconf with new contents
-		if _, err := gitconf.WriteAt(out.Bytes(), 0); err != nil {
-			return fmt.Errorf("could not overwrite .git/config: %s", err)
-		}
-		if err := gitconf.Truncate(int64(out.Len())); err != nil {
-			return fmt.Errorf("could not truncate .git/config: %s", err)
-		}
-		if err := gitconf.Close(); err != nil {
-			return fmt.Errorf("could not close .git/config: %s", err)
-		}
+			fmt.Println("vim-go binaries successfully installed")
+			return nil
+		})
+		// Get the pachyderm repo (slow) & update .git/config in separate goroutine
+		eg.Go(func() error {
+			fmt.Println("Beginning to fetch Pachyderm repo...")
+			op := StartOp()
+			op.Run("go", "get", "github.com/pachyderm/pachyderm")
+			fmt.Println("pachyderm repo fetched")
+			os.Chdir(path.Join(clientpath, "src/github.com/pachyderm/pachyderm"))
 
-		// Get vim-go set up in the new client
-		// TODO: These should just be copied instead of downloaded every time. This
-		// takes like 30 seconds to finish
-		fmt.Println("Installing vim-go binaries...")
-		op.Run("vim", "-c", ":GoInstallBinaries", "-c", ":q")
-		if op.LastError() != nil {
-			return fmt.Errorf("couldn't install go binaries:\n%s", op.DetailedError())
-		}
+			// Create a git branch matching the clientname
+			// TODO(msteffen): let the user specify the branch, in case you want to
+			// have multiple clients for the same non-master branch
+			op.Run("git", "checkout", "-b", clientname)
+			if op.LastError() != nil {
+				return fmt.Errorf("couldn't create client branch:\n%s", op.DetailedError())
+			}
 
-		return nil
+			// Update .git/config so that the 'origin' remote repo uses ssh instead
+			// of http
+			fmt.Println("Updating .git/config...")
+			stat, err := os.Stat("./.git/config")
+			if err != nil {
+				return fmt.Errorf("could not stat .git/config to update origin: %s", err)
+			}
+			gitconf, err := os.OpenFile("./.git/config", os.O_RDWR, 0664)
+			if err != nil {
+				return fmt.Errorf("could not open .git/config to update origin: %s", err)
+			}
+			// (we're replacing one line of config with another that's the same
+			// length)
+			out := bytes.NewBuffer(make([]byte, 0, stat.Size()))
+			scanner := bufio.NewScanner(gitconf)
+			for scanner.Scan() {
+				if scanner.Text() == "\turl = https://github.com/pachyderm/pachyderm" {
+					out.Write([]byte("\turl = git@github.com:pachyderm/pachyderm.git"))
+				} else {
+					out.Write(scanner.Bytes())
+				}
+				out.WriteByte('\n')
+			}
+			// Replace gitconf with new contents
+			if _, err := gitconf.WriteAt(out.Bytes(), 0); err != nil {
+				return fmt.Errorf("could not overwrite .git/config: %s", err)
+			}
+			if err := gitconf.Truncate(int64(out.Len())); err != nil {
+				return fmt.Errorf("could not truncate .git/config: %s", err)
+			}
+			if err := gitconf.Close(); err != nil {
+				return fmt.Errorf("could not close .git/config: %s", err)
+			}
+			return nil
+		})
+
+		// Return once both operations are finished
+		return eg.Wait()
 	}),
 }
 
