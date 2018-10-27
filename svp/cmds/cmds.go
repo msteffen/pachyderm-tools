3e08d39bc283d20f87e225439064107ce71c5148
Matthew Steffen <msteffen@pachyderm.io>
2018-10-27 12:36:06 -0700

    Move RootCmd from cmds.go to svp.go

--- svp/cmds/cmds.go
+++ svp/cmds/cmds.go
@@ -1,11 +1,7 @@
 package cmds
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"io"
-	"log"
 	"os"
 	"path"
 
@@ -70,47 +66,4 @@ func BoundedCommand(minargs, maxargs int, f command) func(*cobra.Command, []stri
 			os.Exit(1)
 		}
 	}
-}
-
-// RootCmd returns the root cobra command (off of which all other svp commands
-// branch).
-func RootCmd() *cobra.Command {
-	// Parse config and initialize config fields
-	configpath := path.Join(os.Getenv("HOME"), ".svpconfig")
-	if _, err := os.Stat(configpath); os.IsNotExist(err) {
-		useDefaultConfig()
-	} else {
-		configfile, err := os.Open(configpath)
-		if err != nil {
-			log.Fatalf("could not open config file at %s for reading: %s",
-				configpath, err)
-		}
-		buf := bytes.NewBuffer(nil)
-		io.Copy(buf, configfile)
-		err = json.Unmarshal(buf.Bytes(), &Config)
-		if err != nil {
-			log.Fatalf("could not parse ${HOME}/.svpconfig: %s", err.Error())
-		}
-	}
-
-	// Initialize git information (current branch, etc)
-	if err := InitGitInfo(); err != nil {
-		fmt.Fprintf(os.Stderr, "could not get info about git repo:\n%s\n(make sure "+
-			"this command is being run from inside a git repo)\n", err.Error())
-		os.Exit(1)
-	}
-
-	// Generate root cobra command & return it
-	root := &cobra.Command{
-		Use: "svp <command>",
-	}
-	root.AddCommand(DiffCommand())
-	root.AddCommand(ChangedFilesCommand())
-	for _, cmd := range GitHelperCommands() {
-		root.AddCommand(cmd)
-	}
-	for _, cmd := range ClientCommands() {
-		root.AddCommand(cmd)
-	}
-	return root
 }
